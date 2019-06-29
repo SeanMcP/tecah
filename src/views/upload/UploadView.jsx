062948dacf78e2510d5a561b91135c9ba081b675
Sean McPherson <seanmcp25@gmail.com>
2019-06-29 14:16:08 -0400

    Upload file

--- src/views/upload/UploadView.jsx
+++ src/views/upload/UploadView.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useAppStore } from 'store/app/AppStore'
+import { uploadDocument } from 'store/app/AppActions'
 
 function UploadView(props) {
+    const [, dispatch] = useAppStore()
+    function handleSubmit(e) {
+        e.preventDefault()
+        const data = new FormData(e.target)
+        const document = data.get('document')
+        if (document) {
+            dispatch(uploadDocument(document))
+            e.target.reset()
+        } else {
+            alert('Select a document to upload')
+        }
+    }
     return (
         <div>
             <h1>Let's upload a document</h1>
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <b>Select document</b>
                     <input type="file" name="document" />
